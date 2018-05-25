389be5814e9a793bb715da39d32d0739f1783efe
gunjianpan <gunjianpan@pinduoduo.com>
2018-05-25 19:49:58 +0800

    R ready

--- R.sh
+++ R.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-IFILE=/home/gunjianpan/桌面/30knot-船舯/*.csv
 OFILE=/home/gunjianpan/桌面/30knot-船舯/resultR.csv
-for file in /home/gunjianpan/桌面/30knot-船舯/R*.csv
+awk - F, '{print "fileName,RX,RY,RZ\n";}'$OFILE >> $OFILE
+for file in R*.csv
 do
 	if [ -r $file ]  #Checks if file exists and readable
 	then
@@ -38,7 +38,7 @@ do
 	  	RX = (sqrt(MAX2 * MAX2) >= sqrt(MIN2 * MIN2)) ? sqrt(MAX2 * MAX2) : sqrt(MIN2 * MIN2);
 	  	RY = (sqrt(MAX3 * MAX3) >= sqrt(MIN3 * MIN3)) ? sqrt(MAX3 * MAX3) : sqrt(MIN3 * MIN3);
 	  	RZ = (sqrt(MAX4 * MAX4) >= sqrt(MIN4 * MIN4)) ? sqrt(MAX4 * MAX4) : sqrt(MIN4 * MIN4);
-	  	print $flie","RX","RY","RZ"\n"
+	  	print FILENAME","RX","RY","RZ"\n"
 	 }' $file >> $OFILE
 	fi
 done
